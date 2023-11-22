5a8ff1bebb18ffe4c2f8103505a4fede225511f9
pradumyadav <pradumyadav020@gmail.com>
2023-11-22 23:24:30 +0530

    updated

--- index.js
+++ index.js
@@ -5,18 +5,49 @@
     app.use(express.json())
 
     const cors =require("cors");
+    const stripe=require("stripe")("sk_test_51OF9HNSGSBjFGMDatfWIxrvEPfOtKJ8TOudE68qT3g5E8ziq4CS9yiGU4uEhz5VyAsaYTZr8H2a2TYUrHjJzZoZi00CnHzpcJA")
+
 const routerForAll = require("./router/routerForAll");
 const connection = require("./configure/db");
 const userRoutes= require("./router/userRoutes")
 
 
     app.use (cors({
-        origin:"*"
+        origin: "*",
+        
     }))
 
     app.use("/api",routerForAll)
     app.use("/user",userRoutes)
 
+//checkOut Api.....
+
+    app.post("/out/create-checkout-session",async(req,res)=>{
+        const{products}=req.body;
+        console.log(products)
+       const lineItems =products.map((product)=>({
+        price_data:{
+            currency:"inr",
+            product_data:{
+
+                name:product.title,
+
+            },
+            unit_amount:product.price * 100
+        },
+        quantity:product.quantity
+       }))
+       
+        const session =await stripe.checkout.sessions.create({
+            payment_method_types:["card"],
+             line_items:lineItems,
+            mode:"payment",
+            success_url:"http://localhost:3000/sucess",
+            cancel_url:"http://localhost:3000/cancel",
+
+        })
+        res.json({id:session.id})
+    })
 
     app.listen(4001,async()=>{
         try{
